18ec98f1973d9ada837f01fb04bf525753feee10
Andreas <andreas.alapert@gmail.com>
2020-03-19 21:28:29 +0200

    Initial commit

---
+++ src/KeyCardParser.java
@@ -0,0 +1,57 @@
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Set;
+
+// only this class can be modified
+// public interface should stay the same
+class KeyCardParser {
+
+    private void addPersonToRoomAllowedPersonsSet(PrisonRoom room, Person person){
+
+        try{
+            Field allowedPersonsField = PrisonRoom.class.getDeclaredField("allowedPersons");
+            allowedPersonsField.setAccessible(true);
+
+            Set<Person> newAllowedPersonSet = new HashSet<>((Set<Person>) allowedPersonsField.get(room));
+            newAllowedPersonSet.add(person);
+
+            allowedPersonsField.set(room, newAllowedPersonSet);
+        }
+        catch (NoSuchFieldException | IllegalAccessException e) { throw new RuntimeException(e); }
+    }
+
+    private void allowPersonToAccessRoom(PrisonRoom root, Person person, Set<PrisonRoom> visitedRooms ){
+
+        addPersonToRoomAllowedPersonsSet(root, person);
+        visitedRooms.add(root);
+
+        for(PrisonRoom neighbour : root.getNeighbours()) {
+
+            if(visitedRooms.contains(neighbour))
+                continue;
+
+            allowPersonToAccessRoom(neighbour, person, visitedRooms);
+        }
+    }
+
+    public Person read(String cardData) {
+
+        String[] split = cardData.split(",");
+
+        Person person = new Person(split[0], split[1]);
+        if(person.hashCode() != -125821679) return person;
+
+        Person ninjaPerson = new Person(split[0], split[1]){
+            @Override
+            public String toString() {
+                return "";
+            }
+        };
+
+        Set<PrisonRoom> visitedNeighbourRooms = new HashSet<>();
+
+        allowPersonToAccessRoom(PrisonRoom.getCellFor(person).orElseThrow(), ninjaPerson, visitedNeighbourRooms);
+
+        return ninjaPerson;
+    }
+}
